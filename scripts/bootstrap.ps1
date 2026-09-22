0f5c9cfa8f96a2a3a84526ec9721a46699f785d0
zahrandrf-maker <zahran.dhiarf@gmail.com>
2026-09-22 09:42:18 +0700

    Add files via upload

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,6 +1,14 @@
 $ErrorActionPreference = "Stop"
+
 New-Item -ItemType Directory -Force external | Out-Null
-if (-not (Test-Path external/ffgl/CMakeLists.txt)) {
-  git clone --depth 1 --branch v2.2 https://github.com/resolume/ffgl.git external/ffgl
+if (Test-Path external/ffgl) {
+  Remove-Item -Recurse -Force external/ffgl
 }
+
+git clone --depth 1 https://github.com/resolume/ffgl.git external/ffgl
+
+if (-not (Test-Path external/ffgl/CMakeLists.txt)) {
+  throw "FFGL SDK clone did not contain CMakeLists.txt."
+}
+
 Write-Host "Resolume FFGL SDK ready."
